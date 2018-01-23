8fcff44d87bdbd9d427437c2e4cbdf178612a56c
Nick <13.divers+lpcki@gmail.com>
2018-01-24 00:31:35 +0100

    Add helper method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,11 @@ class ApplicationController < ActionController::Base
       keys: [:email, contributor_attributes: [:first_name, :last_name]]
     )
   end
+
+  private
+
+  def self.view_accessor(*names)
+    attr_accessor *names
+    helper_method *names
+  end
 end
