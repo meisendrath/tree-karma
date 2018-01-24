496693ba40a2971793513ff6dd2a73a83898cee5
Nick <13.divers+lpcki@gmail.com>
2018-01-24 14:12:52 +0100

    Add filter for contributor & partner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,4 +17,20 @@ class ApplicationController < ActionController::Base
     attr_accessor *names
     helper_method *names
   end
+
+  def contributor_user!
+    redirect_to root_path unless current_user.try(:contributor?)
+  end
+
+  def partner_user!
+    redirect_to root_path unless current_user.try(:partner?)
+  end
+
+  def not_contributor_user!
+    redirect_to root_path if current_user.try(:contributor?)
+  end
+
+  def not_partner_user!
+    redirect_to root_path if current_user.try(:partner?)
+  end
 end
