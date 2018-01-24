2f53e24ad58d2a7eb1b492b13b3fa77069470342
meisendrath <meisendr@gmail.com>
2018-01-24 22:49:45 +0100

    ipfs api file

--- app/services/ruby-ipfs-api.rb
+++ app/services/ruby-ipfs-api.rb
@@ -1,9 +1,12 @@
 # require 'ipfs-api'
+# require 'ipfs-client'
 
 Class IPFS
   class << self
     def upload(receipt)
       ipfs = IPFS::Connection.new
+      client = IPFS::Client.default
+      
       ipfs.add receipt do |node|
       # display each uploaded node:
       p "#{node.name}: #{node.hash}\n" if node.finished?
