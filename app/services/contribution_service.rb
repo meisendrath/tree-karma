bbfaef714fdf38b9707a3182e29f82fdb2aacf7c
Nick <13.divers+lpcki@gmail.com>
2018-01-24 04:02:53 +0100

    Add ContributionService

---
+++ app/services/contribution_service.rb
@@ -0,0 +1,24 @@
+class ContributionService
+  class << self
+    def create_temporal!(user, quantity)
+      price = Money.new(Settings.contribution.price_cents)
+
+      contributions = []
+
+      quantity.times do
+        contributions << Contribution.create!(
+          contributor: user.contributor,
+          price: price
+        )
+      end
+
+      contributions
+    end
+
+    def make_pending!(contributions, stripe_charge_id)
+      contributions.each do |contribution|
+        contribution.update!(status: :pending, stripe_id: stripe_charge_id)
+      end
+    end
+  end
+end
