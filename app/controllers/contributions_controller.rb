036b8ccbfdd34e2e37584242ba6b6385c27af0d3
Nick <13.divers+lpcki@gmail.com>
2018-01-24 01:22:54 +0100

    Add stripe element to view

--- app/controllers/contributions_controller.rb
+++ app/controllers/contributions_controller.rb
@@ -1,3 +1,9 @@
 class ContributionsController < ApplicationController
+  view_accessor :resource
+  
   before_action :authenticate_user!
+
+  def new
+    self.resource = Contribution.new
+  end
 end
