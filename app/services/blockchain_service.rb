e257385301554a9e43693b3a8e70de831901328a
Nick <13.divers+lpcki@gmail.com>
2018-01-24 11:49:12 +0100

    Some advice

--- app/services/blockchain_service.rb
+++ app/services/blockchain_service.rb
@@ -1,49 +1,63 @@
-require BlockchainLite
-require "digest"
+class BlockchainService
+  class << self
 
-class Block
-  attr_reader :index
-  attr_reader :timestamp
-  attr_reader :transactions_count
-  attr_reader :transactions
-  attr_reader :transactions_hash    # merkelroot
-  attr_reader :previous_hash
-  attr_reader :nonce                # ("lucky" number used once) - proof of work if hash starts with leading zeros (00)
-  attr_reader :hash
+    @@blockchain = Blockchain.new
 
-
-  def self.first( data="Genesis" )    # create genesis block
-      ## uses index zero (0) and arbitrary previous_hash ("0")
-      Block.new( 0, data, "0" )
+    def add(contribution)
+      all_blocks = @@blockchain << contribution.to_json
+      b = all_blocks.last
+      # save somewhere this block
+      b
     end
-
-  def self.next( previous, data="Transaction Data..." )
-      Block.new( previous.index+1, data, previous.hash )
-    end
-
-
-end  # class Block
-
-def calculate_hash()
-  sha = Digest::SHA256.new
-    sha.update( @timestamp.to_s +
-                @transactions.to_s +
-                @previous_hash )
-    sha.hexdigest
+  end
 end
 
-# Do we need Proof of Work methods or other mining scenarios?
-
-def add_transaction() #
-
-end
-
-def verify_chain()
-
-end
-
-
-## For storing blocks in a merkle tree...
-  #merkle = Merkletree.for (
-  #JSON data inputs
-)
+# require BlockchainLite
+# require "digest"
+#
+# class Block
+#   attr_reader :index
+#   attr_reader :timestamp
+#   attr_reader :transactions_count
+#   attr_reader :transactions
+#   attr_reader :transactions_hash    # merkelroot
+#   attr_reader :previous_hash
+#   attr_reader :nonce                # ("lucky" number used once) - proof of work if hash starts with leading zeros (00)
+#   attr_reader :hash
+#
+#
+#   def self.first( data="Genesis" )    # create genesis block
+#       ## uses index zero (0) and arbitrary previous_hash ("0")
+#       Block.new( 0, data, "0" )
+#     end
+#
+#   def self.next( previous, data="Transaction Data..." )
+#       Block.new( previous.index+1, data, previous.hash )
+#     end
+#
+#
+# end  # class Block
+#
+# def calculate_hash()
+#   sha = Digest::SHA256.new
+#     sha.update( @timestamp.to_s +
+#                 @transactions.to_s +
+#                 @previous_hash )
+#     sha.hexdigest
+# end
+#
+# # Do we need Proof of Work methods or other mining scenarios?
+#
+# def add_transaction() #
+#
+# end
+#
+# def verify_chain()
+#
+# end
+#
+#
+# ## For storing blocks in a merkle tree...
+#   #merkle = Merkletree.for (
+#   #JSON data inputs
+# )
