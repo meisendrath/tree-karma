5c893898cc853ffc2700f4f2fe84b7d92a56ced0
meisendrath <meisendr@gmail.com>
2018-01-24 23:26:31 +0100

    changes to ipfs rail api file

--- app/services/ruby-ipfs-api.rb
+++ app/services/ruby-ipfs-api.rb
@@ -6,25 +6,14 @@ Class IPFS
     def upload(receipt)
       ipfs = IPFS::Connection.new
       client = IPFS::Client.default
-      
+
       ipfs.add receipt do |node|
       # display each uploaded node:
       p "#{node.name}: #{node.hash}\n" if node.finished?
-    end
+  end
+
+    def our_records(results)
+      results_storage = "#{node.name}: #{node.hash}\n"
+      # Send results_storage back to rails db
+
 end
-
-
-
-
-# folder = '/Users/maxeisendrath/Desktop/ipfs-test'
-
-
-# folder = IPFS::Upload.folder('test') do |test|
-#       test.add_file('hello.txt') do |fd|
-#       fd.write 'Hello'
-#     end
-#
-#     test.add_file('world.txt') do |fd|
-#       fd.write 'World'
-#   end
-# end
