7e85c05b52d32b231a129e4e2b8d647ef6ef7336
meisendrath <meisendr@gmail.com>
2018-01-24 22:37:03 +0100

    ipfs-api file has upload methods

--- app/services/ruby-ipfs-api.rb
+++ app/services/ruby-ipfs-api.rb
@@ -1,17 +1,27 @@
 # require 'ipfs-api'
 
-folder = '/Users/maxeisendrath/Desktop/ipfs-test'
+Class IPFS
+  class << self
+    def upload(receipt)
+      ipfs = IPFS::Connection.new
+      ipfs.add receipt do |node|
+      # display each uploaded node:
+      p "#{node.name}: #{node.hash}\n" if node.finished?
+    end
+end
 
-ipfs = IPFS::Connection.new
-folder = IPFS::Upload.folder('test') do |test|
-  test.add_file('hello.txt') do |fd|
-    fd.write 'Hello'
-  end
-  test.add_file('world.txt') do |fd|
-    fd.write 'World'
-  end
-end
-ipfs.add folder do |node|
-  # display each uploaded node:
-  print "#{node.name}: #{node.hash}\n" if node.finished?
-end
+
+
+
+# folder = '/Users/maxeisendrath/Desktop/ipfs-test'
+
+
+# folder = IPFS::Upload.folder('test') do |test|
+#       test.add_file('hello.txt') do |fd|
+#       fd.write 'Hello'
+#     end
+#
+#     test.add_file('world.txt') do |fd|
+#       fd.write 'World'
+#   end
+# end
